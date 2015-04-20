3c5f0806b861065784e50f1a2d053b373adaafa4
themylogin <themylogin@gmail.com>
2015-04-20 12:44:51 +0600

    More sophisticated objects properties involvement in logic expressions

--- smarthome/config/parser/common/logic_expression.py
+++ smarthome/config/parser/common/logic_expression.py
@@ -1,11 +1,14 @@
 # -*- coding=utf-8 -*-
 from __future__ import absolute_import, division, unicode_literals
 
+import __builtin__
 import logging
+import os
 import re
 
 from smarthome.architecture.object.logic_expression import LogicExpression
 from smarthome.architecture.object.pointer import ObjectPointer, PropertyPointer, PropertyPointerList
+from smarthome.common import DATA_DIR
 
 logger = logging.getLogger(__name__)
 logic_expression_property_regexp = "(?P<object>[a-zA-Z0-9_\[\].]+)\.(?P<property>[a-zA-Z0-9_]+)"
@@ -32,15 +35,36 @@ def parse_constant(val):
 
 
 def parse_logic_expression(expression):
-    properties_involved = []
-
-    def python_property_expression(match):
-        properties_involved.append(PropertyPointer(ObjectPointer(match.group("object")), match.group("property")))
-        return "objects[\"%s\"].get_property(\"%s\")" % (match.group("object"), match.group("property"))
-
-    expression = re.sub(logic_expression_property_regexp, python_property_expression, expression)
+    properties_involved = [PropertyPointer(ObjectPointer(object), property)
+                           for (object, property) in re.findall(logic_expression_property_regexp, expression)]
 
     code = compile(expression + "\n", "<config>", "eval")
-    return LogicExpression(expression=lambda object_manager, **kwargs: eval(code, {"objects": object_manager.objects},
-                                                                            kwargs),
+    return LogicExpression(expression=lambda object_manager, **kwargs: eval(code, {},
+                                                                            LogicExpressionContext(object_manager, dict(
+                                                                                dict(os=os,
+                                                                                     DATA_DIR=DATA_DIR,
+                                                                                     **__builtin__.__dict__),
+                                                                                **kwargs
+                                                                            ))),
                            properties_involved=PropertyPointerList(properties_involved))
+
+
+class LogicExpressionContext(object):
+    def __init__(self, object_manager, kwargs):
+        self.object_manager = object_manager
+        self.kwargs = kwargs
+
+    def __getitem__(self, item):
+        if item in self.kwargs:
+            return self.kwargs[item]
+
+        return LogicExpressionObjectProxy(self.object_manager, item)
+
+
+class LogicExpressionObjectProxy(object):
+    def __init__(self, object_manager, name):
+        self.object_manager = object_manager
+        self.name = name
+
+    def __getattr__(self, item):
+        return self.object_manager.objects[self.name].get_property(item)
